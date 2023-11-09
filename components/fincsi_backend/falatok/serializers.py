20bdc7b0e62f97b5188f2c4485345e6501eb3e77
Zoltán Szabó <szabozoltan6x@gmail.com>
2023-11-09 14:54:37 +0100

    Create serializers for Recipe and User

---
+++ components/fincsi_backend/falatok/serializers.py
@@ -0,0 +1,35 @@
+from rest_framework import serializers
+from .models import Recipe
+from django.contrib.auth.models import User
+
+
+class RecipeSerializer(serializers.HyperlinkedModelSerializer):
+    owner = serializers.ReadOnlyField(source="owner.username")
+    recipe = serializers.HyperlinkedIdentityField(view_name="recipe-detail")
+
+    class Meta:
+        model = Recipe
+        fields = [
+            "id",
+            "url",
+            "recipe",
+            "owner",
+            "title",
+            "ingredients",
+            "description",
+            "directions",
+            "preparation_time",
+            "cooking_time",
+            "photo",
+            "guides",
+        ]
+
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    recipes = serializers.HyperlinkedRelatedField(
+        many=True, view_name="recipe-detail", read_only=True
+    )
+
+    class Meta:
+        model = User
+        fields = ["id", "url", "username", "recipes"]
