262e48e2f0e0f1e445dd2983c54bec7610c12b83
Zoltán Szabó <szabozoltan6x@gmail.com>
2023-11-09 14:54:23 +0100

    Create views for falatok

--- components/fincsi_backend/falatok/views.py
+++ components/fincsi_backend/falatok/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Recipe
+from django.contrib.auth.models import User
+from falatok.permissions import IsUserReadOnly
+from rest_framework import permissions
 
-# Create your views here.
+from falatok.serializers import RecipeSerializer, UserSerializer
+
+
+class RecipeViewSet(viewsets.ModelViewSet):
+    queryset = Recipe.objects.all()
+    serializer_class = RecipeSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsUserReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
