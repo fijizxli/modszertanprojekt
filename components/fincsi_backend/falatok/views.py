7b53efbe0e1c136e442f5b1a0aa7fbd4d5c9dcd0
Zoltán Szabó <szabozoltan6x@gmail.com>
2023-11-11 10:59:38 +0100

    Add filters to RecipeViewSet

--- components/fincsi_backend/falatok/views.py
+++ components/fincsi_backend/falatok/views.py
@@ -3,6 +3,8 @@ from .models import Recipe
 from django.contrib.auth.models import User
 from falatok.permissions import IsUserReadOnly
 from rest_framework import permissions
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter, OrderingFilter
 
 from falatok.serializers import RecipeSerializer, UserSerializer
 
@@ -11,6 +13,11 @@ class RecipeViewSet(viewsets.ModelViewSet):
     queryset = Recipe.objects.all()
     serializer_class = RecipeSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsUserReadOnly]
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    filterset_fields = ["title", "cooking_time", "owner"]
+    search_fields = ["title"]
+    ordering_fields = ["title", "owner"]
+    ordering = ["title"]
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
