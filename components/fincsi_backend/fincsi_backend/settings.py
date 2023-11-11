5750fe2b988636d86d5b9793b1647063c934e439
Zoltán Szabó <szabozoltan6x@gmail.com>
2023-11-11 10:59:18 +0100

    Enable django-filter

--- components/fincsi_backend/fincsi_backend/settings.py
+++ components/fincsi_backend/fincsi_backend/settings.py
@@ -63,6 +63,7 @@ INSTALLED_APPS = [
     # "allauth.socialaccount",
     "drf_spectacular",
     "falatok.apps.FalatokConfig",
+    "django_filters",
 ]
 
 MIDDLEWARE = [
@@ -130,6 +131,7 @@ REST_FRAMEWORK = {
         "rest_framework.authentication.TokenAuthentication",
     ],
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+    "DEFAULT_FILTER_BACKENDS": ["django_filters.rest_framework.DjangoFilterBackend"],
 }
 
 # Password validation
