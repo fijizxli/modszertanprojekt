9441911f8564027ea0bd193d257c5cb9a2b41118
Zoltán Szabó <szabozoltan6x@gmail.com>
2023-11-23 14:41:51 +0100

    Add imports for tests

--- components/fincsi_backend/falatok/tests.py
+++ components/fincsi_backend/falatok/tests.py
@@ -1,3 +1,18 @@
-from django.test import TestCase
+from rest_framework.test import (
+    APITestCase,
+    APIRequestFactory,
+    force_authenticate,
+)
 
-# Create your tests here.
+from django.conf import settings
+from rest_framework import status
+import random
+from django.contrib.auth.models import User
+from .models import Recipe
+from datetime import timedelta
+from .views import RecipeViewSet
+import json as j
+from django.test.client import encode_multipart
+import io
+from PIL import Image
+from django.utils.encoding import force_bytes
