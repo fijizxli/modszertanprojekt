6e6e83d8354c061c760c5f7453a436cf8c35b973
Zoltán Szabó <szabozoltan6x@gmail.com>
2023-11-26 21:52:42 +0100

    Csrf quick fix

---
+++ components/fincsi_backend/falatok/util.py
@@ -0,0 +1,10 @@
+# TODO:SECURITY reenable this; was disabled because we couldnt figure out why csrf (in swagger/js) was interacting badly with SessionMiddleware, search for other instances of this comment
+# https://stackoverflow.com/questions/16458166/how-to-disable-djangos-csrf-validation/47888695#47888695
+class DisableCSRFMiddleware(object):
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        setattr(request, "_dont_enforce_csrf_checks", True)
+        response = self.get_response(request)
+        return response
